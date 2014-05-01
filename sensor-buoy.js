20fbe4b9f368046908f49e03f4b6695eadfeaec2
seshagiri prabhu <seshagiriprabhu@gmail.com>
2014-05-01 11:36:56 +0530

    Improved the display of the project

--- sensor-buoy.js
+++ sensor-buoy.js
@@ -19,11 +19,11 @@ var receipt = true;
 // Set debug to true for more verbose output.
 // login and passcode are optional (required by rabbitMQ)
 var stomp_args ={
-        port: 61613,
-        host: 'localhost',
-        debug: true,
-        login: '',
-        passcode: ''
+    port: 61613,
+    host: 'localhost',
+    debug: true,
+    login: '',
+    passcode: ''
 };
 
 
@@ -39,54 +39,53 @@ function sleep(milliSeconds) {
 }
 
 function genPressure(type) {
-	var pressure = 0.0;
-	switch(type) {
-	  case 'reg-alert':
-		return Math.floor(Math.random() * (104.99 - 95 + 1)) + 95;		
-	  case 'low-alert':
-		return Math.floor(Math.random() * (114.99 - 105 + 1)) + 105;
-	  case 'med-alert':
-		return Math.floor(Math.random() * (129.99 - 115 + 1)) + 115;
-	  case 'high-alert':
-		return Math.floor(Math.random() * (170 - 130 + 1)) + 130;
-	}
+    var pressure = 0.0;
+    switch(type) {
+        case 'reg-alert':
+            return Math.floor(Math.random() * (104.99 - 95 + 1)) + 95;		
+        case 'low-alert':
+            return Math.floor(Math.random() * (114.99 - 105 + 1)) + 105;
+        case 'med-alert':
+            return Math.floor(Math.random() * (129.99 - 115 + 1)) + 115;
+        case 'high-alert':
+            return Math.floor(Math.random() * (170 - 130 + 1)) + 130;
+    }
 }
 
 function genSes(type) {
-	var richter = 0.0;
-	switch(type) {
-          case 'reg-alert':
-		return Math.floor(Math.random() * (2.99 - 0 + 1)) + 0;
-		
-	  case 'low-alert':
-	  	return Math.floor(Math.random() * (3.99 - 3 + 1)) + 3;
-	  case 'med-alert':
-	  	return Math.floor(Math.random() * (5.99 - 4 + 1)) + 4;
-	  case 'high-alert':
-		return Math.floor(Math.random() * (20 - 6 + 1)) + 6;	
-	}
+    var richter = 0.0;
+    switch(type) {
+        case 'reg-alert':
+            return Math.floor(Math.random() * (2.99 - 0 + 1)) + 0;
+        case 'low-alert':
+            return Math.floor(Math.random() * (3.99 - 3 + 1)) + 3;
+        case 'med-alert':
+            return Math.floor(Math.random() * (5.99 - 4 + 1)) + 4;
+        case 'high-alert':
+            return Math.floor(Math.random() * (10 - 6 + 1)) + 6;	
+    }
 }
 
 
 client.on('connected', function() {
-    
-    while(true) {
-	 var p = genPressure(alert);
-	 var r = genSes(alert);
-	 
-	 var param = '{\"buoy\": \"'+buoy+'\", \"pressure\": '+p+', \"ritcher\": '+r+'}';
 
-	//console.log(param);
-        client.send({
-            'destination': queue,
-            'body': param,
-            'persistent': 'true'
-        }, receipt);
-        sleep(1000);
-    }
-    //console.log('Produced');
-    client.disconnect();
-});
+    while(true) {
+        var p = genPressure(alert);
+        var r = genSes(alert);
+
+        var param = '{\"buoy\": \"'+buoy+'\", \"pressure\": '+p+', \"ritcher\": '+r+'}';
+
+            //console.log(param);
+            client.send({
+                'destination': queue,
+                'body': param,
+                'persistent': 'true'
+            }, receipt);
+            sleep(1000);
+        }
+        //console.log('Produced');
+        client.disconnect();
+    });
 
 client.on('receipt', function(receipt) {
     console.log("ACK: " + receipt);
--- server.js
+++ server.js
@@ -23,13 +23,13 @@ var sys = require('util');
 //var buoyList = new Array();
 
 var server = http.createServer();
-    
+
 var stomp_args ={
-        port: config.stomp.port,
-        host: config.stomp.host,
-        debug: config.stomp.debug,
-        login: config.stomp.login,
-        passcode: config.stomp.pass
+    port: config.stomp.port,
+    host: config.stomp.host,
+    debug: config.stomp.debug,
+    login: config.stomp.login,
+    passcode: config.stomp.pass
 }
 
 var pos = "{\"buoy1\": {\"lat\": 9.092732, \"lng\": 72.494087 }, \"buoy2\": { \"lat\": 8.092732, \"lng\": 74.494087 }, \"buoy3\": { \"lat\": 12.092732, \"lng\": 76.494087 }, \"buoy4\": { \"lat\": 7.092732, \"lng\": 84.494087 }}";
@@ -45,8 +45,8 @@ var headers = { destination: '/queue/Buoys' };
 // Server providing web-socket service
 server.listen(8080,function() {
     console.log('\nTsunami Warning System Server');
-	console.log('\nRunning on http://0.0.0.0:8080/');
-	console.log('\nMonitor Service on http://localhost/tws/index.html')
+    console.log('\nRunning on http://0.0.0.0:8080/');
+    console.log('\nMonitor Service on http://localhost/tws/index.html')
 });
 
 // Attach the web-socket to server
@@ -54,52 +54,42 @@ io = sio.listen(server);
 
 // On connection from Buoys //
 io.sockets.on('connection', function(socket) {
-        console.log('Monitoring system connected');		
+    console.log('Monitoring system connected');		
 
-        socket.on('disconnect',function(){
-                console.log('Monitoring system disconnected');
-	});
+    socket.on('disconnect',function(){
+        console.log('Monitoring system disconnected');
+    });
 });
 // Connection to Central Message Broker
 client.connect(); 
 
 // Subscribe to messages from Queue
 client.on('connected', function() {
-        client.subscribe(headers);
-	console.log('Subscribed to buoys queue');
-	
+    client.subscribe(headers);
+    console.log('Subscribed to buoys queue');
 });
 
 // Data to web-socket for Monitoring system
 client.on('message', function(message) {
-        
-	var signal = JSON.parse(message.body);
-	
-	io.sockets.volatile.emit('active-buoy', {data: signal.buoy});
-	//io.sockets.volatile.emit('sensor-position', {data: pos});
-		
-	
-	if ((signal.pressure > 95 && signal.pressure < 104.99) &&  (signal.ritcher > 0 && signal.ritcher < 2.99) ){
-	
-		io.sockets.volatile.emit('regular', {data: message.body});
-	
-	}
-	if ((signal.pressure > 105 && signal.pressure < 114.99) && (signal.ritcher > 3 && signal.ritcher < 3.99)) {
-        	io.sockets.volatile.emit('low-alert', {data: message.body});
-		
-	}
-    	if ((signal.pressure > 115 && signal.pressure < 129.99) && (signal.ritcher > 4 && signal.ritcher < 5.99)) {
-        	io.sockets.volatile.emit('med-alert', {data: message.body});
-	}
-    	if ((signal.pressure > 130 && signal.pressure < 170) && (signal.ritcher > 5 && signal.ritcher < 20)) {
-        
-		io.sockets.volatile.emit('high-alert', {data: message.body});
-	}
-	
-     
+    var signal = JSON.parse(message.body);
+    io.sockets.volatile.emit('active-buoy', {data: signal.buoy});
+    //io.sockets.volatile.emit('sensor-position', {data: pos});
+
+    if ((signal.pressure > 95 && signal.pressure < 104.99) &&  (signal.ritcher > 0 && signal.ritcher < 2.99) ){
+        io.sockets.volatile.emit('reg-alert', {data: message.body});
+    }
+    if ((signal.pressure > 105 && signal.pressure < 114.99) && (signal.ritcher > 3 && signal.ritcher < 3.99)) {
+        io.sockets.volatile.emit('low-alert', {data: message.body});
+    }
+    if ((signal.pressure > 115 && signal.pressure < 129.99) && (signal.ritcher > 4 && signal.ritcher < 5.99)) {
+        io.sockets.volatile.emit('med-alert', {data: message.body});
+    }
+    if ((signal.pressure > 130 && signal.pressure < 170) && (signal.ritcher > 5 && signal.ritcher < 10)) {
+        io.sockets.volatile.emit('high-alert', {data: message.body});
+    }
 });
 
 client.on('error', function(error_frame) {
-        console.log(error_frame.body);
-        client.disconnect();
+    console.log(error_frame.body);
+    client.disconnect();
 });
