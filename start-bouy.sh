df8066b21996f80ba441da1f361ed2c84088b5e9
premsankar-au <premsankar-au@users.noreply.github.com>
2014-05-02 17:34:37 +0530

    update start-bouy.sh
    starting all the sensors in background

--- start-bouy.sh
+++ start-bouy.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+nohup node sensor-buoy.js reg-alert bouy1 --config config.json &
+nohup node sensor-buoy.js low-alert bouy2 --config config.json &
+nohup node sensor-buoy.js med-alert bouy3 --config config.json &
+nohup node sensor-buoy.js high-alert bouy4 --config config.json &
+
+rm nohup.out
